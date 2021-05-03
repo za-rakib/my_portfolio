cfc94d186b875a4061901f285278ff419206af33
Rakib <phpzarakib@gmail.com>
2021-05-04 03:44:49 +0600

    commit

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h1 className="text-center">Blog is coming soon.  </h1>
+            <h1 className="text-center">Blog is coming soon. </h1>
+            <h2>Just wt</h2>
         </div>
     );
 };
